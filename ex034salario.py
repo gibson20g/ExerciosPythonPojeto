947f9deedff33f913e0e25a11d40d48516e2519d
gibson20g <77676359+gibson20g@users.noreply.github.com>
2021-03-27 10:25:33 -0300

    Atualização de Codigos ex019 e ex034

--- ex034salario.py
+++ ex034salario.py
@@ -1,6 +1,27 @@
-salario = float(input("Qual é o salário do funcionario?R$"))
-if salario <= 1250:
-	novo = salario + (salario * 15 / 100)
-else:
-	novo = salario + (salario * 10 / 100)
-print("Quem ganhava R${:.2f} passa a ganhar R$ {:.2f} agora".format(salario, novo))
+nome = str(input("Digite o Nome do Funcionario: "))
+salario = float(input("Qual é o salário do funcionario?R$: "))
+
+if salario <= 1045:
+	novo = salario + (salario * 5.26 / 100)
+	porcent = 5.26
+	aumento = novo - salario
+	print("O salario antigo é R${:.2f}, Porcentagem de aumento {}% ".format(salario, porcent))
+	print("aumento de R${:.2f}, E o novo salário será {:.2f}".format(aumento, novo))
+elif 2195 >= salario > 1045:
+	novo = salario + (salario * 4.32 / 100)
+	porcent = 4.32
+	aumento = novo - salario
+	print("O salario antigo é R${:.2f}, Porcentagem de aumento {}% ".format(salario, porcent))
+	print("aumento de R${:.2f}, E o novo salário será {:.2f}".format(aumento, novo))
+elif 3500 > salario > 2195:
+	novo = salario + (salario * 2.75 / 100)
+	porcent = 2.75
+	aumento = novo - salario
+	print("O salario antigo é R${:.2f}, Porcentagem de aumento {}% ".format(salario, porcent))
+	print("aumento de R${:.2f}, E o novo salário será {:.2f}".format(aumento, novo))
+elif salario >= 3500:
+	novo = salario + (salario * 1.79 / 100)
+	porcent = 1.79
+	aumento = novo - salario
+	print("O salario antigo é R${:.2f}, Porcentagem de aumento {}% ".format(salario, porcent))
+	print("aumento de R${:.2f}, E o novo salário será {:.2f}".format(aumento, novo))
